54acf1569f915810b0252930ac9b2ccff608b02d
Michael Eco <michael.gunawan2002@gmail.com>
2025-08-26 13:19:27 +0700

    feat: added Block component

---
+++ block.py
@@ -0,0 +1,25 @@
+import time
+import hashlib
+from typing import Any
+
+class Block:
+    def __init__(self, index: int, data: Any, previous_hash: Any) -> None:
+        self.index = index
+        self.timestamp = time.time()
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.compute_hash()
+
+    def compute_hash(self):
+        block_string = str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash)
+        encoded_block_string = block_string.encode()
+        sha256 = hashlib.sha256()
+        sha256.update(encoded_block_string)
+        return sha256.hexdigest()
+
+    def __repr__(self):
+        return f"""Block(Index: {self.index}, Timestamp: {self.timestamp}, Data: {self.data}, Hash: {self.hash}. Previous Hash: {self.previous_hash})"""
+
+if __name__ == "__main__":
+    genesis_block = Block(0, "Genesis Block", "0000000000000000000000000000000000000000000000000000000000000000")
+    print(genesis_block)
