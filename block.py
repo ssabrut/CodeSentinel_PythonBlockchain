ac9ed154dcc5b44f92e60e9c1d257271154e9d71
Michael Eco <michael.gunawan2002@gmail.com>
2025-08-26 13:27:30 +0700

    feat: implemented Blockchain class with create_genesis_block, get_latest_block, and add_block functions

--- block.py
+++ block.py
@@ -10,16 +10,17 @@ class Block:
         self.previous_hash = previous_hash
         self.hash = self.compute_hash()
 
-    def compute_hash(self):
+    def compute_hash(self) -> str:
         block_string = str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash)
         encoded_block_string = block_string.encode()
         sha256 = hashlib.sha256()
         sha256.update(encoded_block_string)
+        print(type(sha256.hexdigest()))
         return sha256.hexdigest()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"""Block(Index: {self.index}, Timestamp: {self.timestamp}, Data: {self.data}, Hash: {self.hash}. Previous Hash: {self.previous_hash})"""
 
 if __name__ == "__main__":
-    genesis_block = Block(0, "Genesis Block", "0000000000000000000000000000000000000000000000000000000000000000")
+    genesis_block = Block(index=0, data="Genesis Block", previous_hash="0000000000000000000000000000000000000000000000000000000000000000")
     print(genesis_block)
---
+++ blockchain.py
@@ -0,0 +1,19 @@
+from block import Block
+from typing import Any
+
+class Blockchain:
+    def __init__(self) -> None:
+        self.chain = [self.create_genesis_block()]
+
+    def create_genesis_block(self) -> Block:
+        return Block(0, "Genesis Block", "0000000000000000000000000000000000000000000000000000000000000000")
+
+    def get_latest_block(self) -> Block:
+        return self.chain[-1];
+
+    def add_block(self, data: Any) -> None:
+        latest_block = self.get_latest_block()
+        new_index = latest_block.index + 1
+        previous_hash = latest_block.hash
+        new_block = Block(index=new_index, data=data, previous_hash=previous_hash)
+        self.chain.append(new_block)
